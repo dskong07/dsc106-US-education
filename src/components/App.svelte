c664dec080951a613471d860d141d3d431b00acb
Daniel Kong <37390751+dkong07@users.noreply.github.com>
2024-02-15 19:42:39 -0800

    components

---
+++ src/components/App.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { json } from "d3";
+    import Marks from "./Marks.svelte";
+    let dataset = [];
+    json(
+      "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
+    ).then((data) => {
+      dataset = data.features;
+    });
+    const width = 1200,
+      height = 600;
+  </script>
+  
+  <main>
+    <svg {width} {height}>
+      <Marks {dataset} />
+    </svg>
+  </main>
