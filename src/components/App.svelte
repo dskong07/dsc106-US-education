6bf5a0c6a7f487b2cbaa63268055ab05a2f26f72
Daniel Kong <37390751+dkong07@users.noreply.github.com>
2024-02-15 23:58:26 -0800

    Update on map functions
    working map hover, data get, select

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,18 +1,43 @@
 <script>
   import { json } from "d3";
-  import Marks from "./Marks.svelte";
+  import { onMount } from 'svelte';
+  import Interactive_Map from "./Interactive_Map.svelte";
+  
   let dataset = [];
-  json(
-    "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
-  ).then((data) => {
-    dataset = data.features;
-  });
-  const width = 1200,
-    height = 600;
+  onMount(async () => {
+		const us = await fetch('https://raw.githubusercontent.com/dkong07/dsc106-p3/main/us-states.json')
+			.then(d => d.json())
+
+		dataset = us.features
+		console.log({ dataset })
+	})
 </script>
 
 <main>
-  <svg {width} {height}>
-    <Marks {dataset} />
-  </svg>
+
+
+  <section class="graph">
+    <h2 style="margin-top: 15px">USA</h2>
+    <Interactive_Map {dataset}/>
+  
+  </section>
 </main>
+
+<style>    
+.main {
+  text-align: center;
+  font-family: "Nunito", sans-serif;
+  font-weight: 300;
+  line-height: 2;
+  font-size: 24px;
+  color: var(--color-text);
+  margin-top: 100px;
+}
+.graph {
+  text-align: center;
+  display: inline-block;
+  margin-left: 0px;
+  width:1200px;
+  height:700px;
+    }
+</style>
---
+++ src/components/Interactive_Map.svelte
@@ -0,0 +1,91 @@
+<script>
+
+	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
+	import { geoPath, geoAlbersUsa } from 'd3-geo';
+	import { draw } from 'svelte/transition';
+	export let dataset = [];
+	const projection = geoAlbersUsa()	
+	const path = geoPath(projection)
+	let selected;
+	let hovered = -1; 
+	let recorded_mouse_position = {
+		x: 0, y: 0
+	};
+	
+	
+
+	
+</script>
+<div class="visualization">
+    <svg viewBox="0 0 1200 610">
+        <g fill="white" stroke="black">
+            {#each dataset as feature,i}
+                <path 
+                d={path(feature)} 
+                fill={i===hovered ? "hsl(0 0% 50% / 20%)" : "white"}
+                on:click={() => selected = feature} 
+                on:mouseover={(event) =>{
+                    hovered = i; recorded_mouse_position = {
+                        x: event.pageX,
+                        y: event.pageY
+                    }
+                    console.log(i, recorded_mouse_position)
+                }}
+                on:mouseout={(event) => { hovered = -1; }}
+                class="state" 
+                in:draw={{ delay: 0, duration: 1000 }} />
+            {/each}
+        </g>		
+        {#if selected}
+            <path d={path(selected)} fill="hsl(0 5% 50% / 60%)" stroke="black" stroke-width={2} />
+        {/if}
+    </svg>
+</div>
+
+
+<div class="tooltip-selected">
+	State selected: {selected?.properties.name ?? 'Pick a state!'}
+</div>
+
+<div	
+class={hovered === -1 ? "tooltip-hidden": "tooltip-visible"}
+style="left: {recorded_mouse_position.x + 40}px; top: {recorded_mouse_position.y + 40}px">
+	{#if hovered !== -1}
+		this be {dataset[hovered].properties.name}
+	{/if}
+</div>
+	
+<style>
+    .visualization {
+        font: 25px sans-serif;
+		margin: auto;
+		margin-top: 1px;
+		text-align: middle;
+    }
+	.tooltip-selected {
+		text-align: center;
+		margin-top: 8px;
+		font-size: 1.5rem;
+	}
+	.tooltip-hidden {
+		visibility: hidden;
+		font-family: "Nunito", sans-serif;
+		width: 200px;
+		position: absolute;
+	}
+
+	.tooltip-visible {
+		font: 25px sans-serif;
+		font-family: "Nunito", sans-serif;
+		visibility: visible;
+		background-color: #f0dba8;
+		border-radius: 10px;
+		width: 200px;
+		color: black;
+		position: absolute;
+		padding: 10px;
+	}
+</style>
+
+<!---->
