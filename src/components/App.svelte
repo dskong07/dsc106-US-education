9cfba2dfffd262f563dc49efffa50ee0413a67cd
Daniel Kong <37390751+dkong07@users.noreply.github.com>
2024-02-16 00:19:22 -0800

    including dataset

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -2,15 +2,27 @@
   import { json } from "d3";
   import { onMount } from 'svelte';
   import Interactive_Map from "./Interactive_Map.svelte";
+  import { read, utils, writeFile } from 'xlsx';
   
   let dataset = [];
   onMount(async () => {
 		const us = await fetch('https://raw.githubusercontent.com/dkong07/dsc106-p3/main/us-states.json')
 			.then(d => d.json())
-
 		dataset = us.features
 		console.log({ dataset })
 	})
+
+  onMount(async () => {
+    const f = await (await fetch("https://github.com/dkong07/dsc106-p3/blob/630bc47b9418d7d68af7eeaf8c94e9c7436fa446/retention_y2.xlsx")).arrayBuffer();
+    const wb = read(f);
+    const gradeinfo = utils.sheet_to_json<Grade>(wb.Sheets[wb.SheetNames[0]]);
+    console.log(gradeinfo);
+
+
+  })
+  
+
+
 </script>
 
 <main>
