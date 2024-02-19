1ee084c5c775e1caaaaa79642ad86bd449bb6ef6
moonish1211 <jillnomura1211@gmail.com>
2024-02-19 15:08:35 -0800

    'wrong'

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -23,27 +23,41 @@
   })
   
   //This is for the checkmark, Yes for yes clicking
+  //https://svelte.dev/repl/fd38cae55917408aa43ed9a98425a587?version=3.50.1 
   let yes = true;
+  const MAX_SELECTED_ITEMS = 2; // Maximum number of items that can be selected at once.
+	const MAX_AVAILABLE_OPTIONS = 10; // How many options should there be in total.
+	
+	// The list of selected items that we will bind to the input group.
+	let selectedItems = [];
+	
+	// Initiate an array containing the numbers 0 - MAX_AVAILABLE_OPTIONS
+	let availableOptions = Array.from(Array(MAX_AVAILABLE_OPTIONS), (_, i) => i+1);
+	
+	// This is where we will store our selection.
+	let chosenItems = [];
+
+	// Update the selected items if the array of selected items is changed.
+	const updateSelectedItems = () => {
+			// Remove any items from `chosenItems` that are no longer selected.
+			chosenItems = chosenItems.filter(x => selectedItems.includes(x))
+		
+			// Make sure that the new items do not include any already selected items.
+			let newItems = selectedItems.filter(x => !chosenItems.includes(x))
+
+			// Merge the previously chosen items, with the newly selected items. 
+			chosenItems = [...chosenItems, ...newItems].slice(-MAX_SELECTED_ITEMS);
+
+			// And now update the group of selected items to only include the once that are chose.
+			selectedItems = selectedItems.filter(x => chosenItems.includes(x))
+	}
+	console.log({selectedItems})
+  console.log({chosenItems})
+
+
 
 </script>
 
-<!-- Adding a checkmark for gender, THE default is total which is denoted by clicking both Male and Female -->
-<div class="gender_filter">
-  <h3>Filter based on Gender</h3>
-	<label>
-    <input type="checkbox" bind:checked={yes} />
-    Female
-  </label>
-  <label>
-    <input type="checkbox" bind:checked={yes} />
-    Male
-  </label>
-</div>
-
-<!-- A button to apply filter -->.
-<Button class="primary lg">
-	Apply Filter
-</Button>
 
 <main>
   <section class="graph">
@@ -52,6 +66,32 @@
   
   </section>
 </main>
+
+<!-- Grouping component for filtering (hopefully we can move this to the right) -->
+<div class = "filter"> 
+  <!-- Adding a checkmark for gender, THE default is total which is denoted by clicking both Male and Female -->
+  <div class="gender_filter">
+    <h2>Filter based on Gender</h2>
+
+    {#each availableOptions as value}
+    <label>
+      <input
+				 type="checkbox"
+				 name="items"
+				 value={value}	 
+				 bind:group={selectedItems}
+				 on:change={updateSelectedItems}
+      />
+      <span>Item {value}</span>
+    </label>
+    {/each}
+  </div>
+  
+  <!-- A button to apply filter -->.
+  <Button class="primary lg">
+    Apply Filter
+  </Button>
+</div>
 
 <style>    
 .main {
@@ -70,4 +110,19 @@
   width:1200px;
   height:700px;
     }
+
+.filter {
+  display: inline-block;
+  
+}
+.gender_filter{
+  padding-bottom: 25px;
+}
+
+label {
+		user-select: none;
+	}
+	label:has(input[type="checkbox"]:checked){
+		color:green
+	}
 </style>
